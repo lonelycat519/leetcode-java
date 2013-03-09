85f17985ae8bc5d616cf67470a81a41932678c61
lonelycat519 <lonelycat519@gmail.com>
2013-03-08 16:57:52 -0800

    Create searchInsert.java

---
+++ searchInsert.java
@@ -0,0 +1,26 @@
+/* Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ 
+     You may assume no duplicates in the array.
+     
+     Here are few examples.
+     [1,3,5,6], 5 → 2
+     [1,3,5,6], 2 → 1
+     [1,3,5,6], 7 → 4
+     [1,3,5,6], 0 → 0 */
+
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        int len = A.length;
+        
+        if(len==0)  return 0;
+        
+        // Searching...
+        for(int i=0; i<len; i++){
+            if(A[i]==target || A[i]>target)
+                return i;
+        }
+        
+        // Target is larger than any element in the array...
+        return len;
+    }
+}
